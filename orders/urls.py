095a3f336669027823523b0ccfca7deb0f01eae7
maksim <maksim-paxomov-2000@mail.ru>
2025-02-18 16:04:25 +0300

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -7,11 +7,11 @@ from orders.views import (OrderCreateAPIView, OrderListAPIView, OrderDestroyAPIV
 app_name = OrdersConfig.name
 
 urlpatterns = [
-    path('api/add/', OrderCreateAPIView.as_view(),name='add_order'),
-    path('api/list_orders/', OrderListAPIView.as_view(),name='list_order'),
-    path('api/retrieve_order/<int:pk>/', OrderRetrieveAPIView.as_view(),name='add_order'),
-    path('api/edit_order/<int:pk>/', OrderUpdateAPIView.as_view(), name='edit_order'),
-    path('api/delete_order/<int:pk>/', OrderDestroyAPIView.as_view(), name='delete_order'),
+    path('api/add/', OrderCreateAPIView.as_view(),name='add_order_api'),
+    path('api/list_orders/', OrderListAPIView.as_view(),name='order_list_api'),
+    path('api/retrieve_order/<int:pk>/', OrderRetrieveAPIView.as_view(),name='retrieve_order_api'),
+    path('api/edit_order/<int:pk>/', OrderUpdateAPIView.as_view(), name='edit_order_api'),
+    path('api/delete_order/<int:pk>/', OrderDestroyAPIView.as_view(), name='delete_order_api'),
 
     path('', OrderListView.as_view(), name='order_list'),
     path('add/', OrderCreateView.as_view(), name='add_order'),
